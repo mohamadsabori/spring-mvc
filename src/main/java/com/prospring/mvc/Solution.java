913f1549ec71a47978619181c05da146eca4d227
mohammad <m.sabori.86@gmail.com>
2025-05-22 20:53:45 +0200

    Solution of this topic: Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
    The overall run time complexity should be O(log (m+n)).
    
    Example 1:
    
    Input: nums1 = [1,3], nums2 = [2]
    Output: 2.00000
    Explanation: merged array = [1,2,3] and median is 2.
    Example 2:
    
    Input: nums1 = [1,2], nums2 = [3,4]
    Output: 2.50000
    Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
    
    Constraints:
    
    nums1.length == m
    nums2.length == n
    0 <= m <= 1000
    0 <= n <= 1000
    1 <= m + n <= 2000
    -106 <= nums1[i], nums2[i] <= 106
    
    https://leetcode.com/problems/median-of-two-sorted-arrays/submissions/1641533887

---
+++ src/main/java/com/prospring/mvc/Solution.java
@@ -0,0 +1,52 @@
+package com.prospring.mvc;
+
+class Solution {
+    public static void main(String[] args) {
+        int[] nums1 = {0,0};
+        int[] nums2 = {0,0};
+        System.out.println(findMedianSortedArrays(nums1, nums2));
+    }
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // Ensure nums1 is the smaller array
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int x = nums1.length;
+        int y = nums2.length;
+
+        // Edge case: both arrays are empty
+        if (x == 0 && y == 0) {
+            throw new IllegalArgumentException("Both arrays are empty");
+        }
+
+        int low = 0, high = x;
+
+        while (low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = (x + y + 1) / 2 - partitionX;
+
+            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
+            int minRightX = (partitionX == x) ? Integer.MAX_VALUE : nums1[partitionX];
+
+            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
+            int minRightY = (partitionY == y) ? Integer.MAX_VALUE : nums2[partitionY];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                if ((x + y) % 2 == 0) {
+                    return ((double) Math.max(maxLeftX, maxLeftY) +
+                            Math.min(minRightX, minRightY)) / 2;
+                } else {
+                    return Math.max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX > minRightY) {
+                high = partitionX - 1;
+            } else {
+                low = partitionX + 1;
+            }
+        }
+
+        return 0L;
+    }
+}
